387991a407bddc2459cc9361350621928b7bca14
filip <filip@baving@users.noreply.github.com>
2025-11-10 11:49:32 +0100

    stuff

--- public/app.js
+++ public/app.js
@@ -1,8 +1,4 @@
 
-console.log("Hello world");
-
-console.log("This is a test file.");
-
 document.getElementById("helloBtn").addEventListener("click", () => {
   document.getElementById("output").textContent = "Button clicked by Kim!";
 });

2410732f3c0d5206a0d9b6f63de1c1930dbc1b2a
newstki <164892807+newstki@users.noreply.github.com>
2025-11-21 11:47:59 +0100

    fixat inlämningar

--- public/canvas_test.js
+++ public/canvas_test.js
@@ -8,27 +8,7 @@ const btn = document.getElementById("testLoadResultat");
 
 
 
-// När användaren väljer en modul -> hämta inlämningar
-kursSelect.addEventListener("change", async () => {
-    const kurskod = kursSelect.value;
 
-    if (!kurskod) {
-        inlamningSelect.innerHTML = '<option>-- Välj kurs först --</option>';
-        return;
-    }
-
-    const res = await fetch(`/canvas/kurs/${kurskod}/inlamningar`);
-    const inlamningar = await res.json();
-
-    inlamningSelect.innerHTML = '<option value="">-- Välj inlämning --</option>';
-
-    inlamningar.forEach(i => {
-        const opt = document.createElement("option");
-        opt.value = i.inlamningsid;
-        opt.textContent = i.titel;
-        inlamningSelect.appendChild(opt);
-    });
-});
 
 
 //  Ladda studentresultat 
